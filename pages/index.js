20a40dc37f82c7292d2429b0efbe11271db104b4
Vlad1m1ru5 <kubantsev.vov@mail.ru>
2021-04-20 12:35:13 +0300

    feature home page MVP

--- pages/index.js
+++ pages/index.js
@@ -3,60 +3,75 @@ import Head from "next/head";
 
 export default function Home() {
   return (
-    <div className="min-h-screen flex flex-col">
+    <div className="min-h-screen flex flex-col bg-gray-50">
       <Head>
-        <title>Create Next App</title>
+        <title>Docs App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <header>
+      <header className="py-4 bg-white">
         <Container>
-          <h1 className="">
-            Welcome to <a href="https://nextjs.org">Next.js!</a>
-          </h1>
+          <div className="flex flex-wrap items-center justify-between">
+            <h1 className="text-4xl font-bold">Docs App</h1>
+            <form action="/login">
+              <button
+                type="submit"
+                className="ml-2 px-2 py-2 text-lg bg-green-400 text-white rounded outline-none focus:bg-green-700"
+              >
+                Login
+              </button>
+            </form>
+          </div>
         </Container>
-        <hr />
       </header>
+      <hr />
       <main className="flex-1">
-        <p className="">
-          Get started by editing <code className="">pages/index.js</code>
-        </p>
-        <div className="">
-          <a href="https://nextjs.org/docs" className="">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-          <a href="https://nextjs.org/learn" className="">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className=""
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className=""
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-      <footer className="">
-        <hr />
         <Container>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Powered by <img src="/vercel.svg" alt="Vercel Logo" className="" />
-          </a>
+          <div className="flex flex-col items-center space-y-2 mt-8">
+            <form action="" className="flex flex-row w-full">
+              <input
+                type="search"
+                placeholder="Search the Docs"
+                className="px-2 py-2 w-full rounded outline-none focus:border focus:border-blue-700 focus:-m-px"
+              />
+              <button
+                type="submit"
+                className="ml-2 px-2 py-2 text-lg bg-blue-400 text-white rounded outline-none focus:bg-blue-700"
+              >
+                Search
+              </button>
+            </form>
+            <a
+              href="/"
+              className="px-2 py-2  text-lg bg-blue-400 text-white rounded outline-none focus:bg-blue-700"
+            >
+              Add Document
+            </a>
+          </div>
+        </Container>
+      </main>
+      <hr />
+      <footer className="py-4 bg-white">
+        <Container>
+          <div className="flex flex-wrap justify-around">
+            <span className="text-2xl">
+              Welcome to{" "}
+              <a
+                href="https://nextjs.org"
+                className="outline-none focus:underline"
+              >
+                Next.js!
+              </a>
+            </span>
+            <a
+              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex items-center text-2xl outline-none focus:underline"
+            >
+              Powered by{" "}
+              <img src="/vercel.svg" alt="Vercel Logo" className="ml-2 h-4" />
+            </a>
+          </div>
         </Container>
       </footer>
     </div>
