f9e1f2d2e0ac332bbeaf9ae7bb3d718ff9eb9369
Vlad1m1ru5 <kubantsev.vov@mail.ru>
2021-05-03 18:02:34 +0300

    Feaure postgres with elasticsearch index for text search

--- pages/api/docs.js
+++ pages/api/docs.js
@@ -1,3 +1,12 @@
+import pool, { selectDocument } from "@database/pool";
+
 export default (req, res) => {
-  return res.status(200).json(["one"]);
+  pool.query(selectDocument, [req.body.search], (err, result) => {
+    if (err) {
+      console.log("Error in query", err.stack);
+      return res.status(500).json({ docs: [], message: err.message });
+    }
+
+    return res.status(200).json({ docs: result.rows, message: "" });
+  });
 };
--- pages/api/upload.js
+++ pages/api/upload.js
@@ -1,16 +1,82 @@
+import pool, { insertDistribution, insertDocument } from "@database/pool";
 import { IncomingForm } from "formidable";
+import fs from "fs";
+import matter from "gray-matter";
 
 export default function (req, res) {
-  const incomingForm = new IncomingForm();
-  incomingForm.parse(req, (err, fields, files) => {
+  new IncomingForm().parse(req, (err, fields, files) => {
     if (err) {
       const { message } = err;
       return res.status(500).json({ message });
     }
 
-    console.log(files);
+    const { file } = files;
+    if (file === null) {
+      return res.status(500).json({ message: "Error parsing file form field" });
+    }
+
+    const { name, path } = file;
+    const fileContents = fs.readFileSync(path).toString();
+    const { data, content } = matter(fileContents);
+    const { groupId, artifactId, version } = data;
+
+    if (groupId === null || artifactId === null || version === null) {
+      return res.status(500).json({ message: "Error parsing front matter" });
+    }
+
+    pool.connect((err, client, done) => {
+      const shouldAbort = (err) => {
+        if (err) {
+          console.log("ERROR in transaction", err.stack);
+          client.query("ROLLBACK", (err) => {
+            err && console.log("Error rolling back", err.stack);
+            done();
+          });
+        }
+        return !!err;
+      };
+
+      if (shouldAbort(err)) {
+        return res.status(500).json({ message: err.message });
+      }
+
+      client.query("BEGIN", (err) => {
+        if (shouldAbort(err)) {
+          return res.status(500).json({ message: err.message });
+        }
+
+        client.query(
+          insertDistribution,
+          [groupId, artifactId, version],
+          (err) => {
+            if (shouldAbort(err)) {
+              return res.status(500).json({ message: err.message });
+            }
+
+            client.query(
+              insertDocument,
+              [name, groupId, artifactId, version, content],
+              (err) => {
+                if (shouldAbort(err)) {
+                  return res.status(500).json({ message: err.message });
+                }
+
+                client.query("COMMIT", (err) => {
+                  if (err) {
+                    console.log("Error commiting transaction", err.stack);
+                    res.status(500).json({ message: err.message });
+                  }
+
+                  done();
+                  return res.status(200).json({ message: "Uploaded" });
+                });
+              }
+            );
+          }
+        );
+      });
+    });
   });
-  return res.status(200).json({ message: "Uploaded" });
 }
 
 export const config = {
